cab36c12aef9722a8699bb3ba76ef343adcbd41e
uzairmahmed <uzairmahmed@gmail.com>
2017-07-04 19:04:49 -0400

    Added Testing and Troubleshooting Sketches
    July 4, 2017

---
+++ Testing/LightStrip/LightStrip.ino
@@ -0,0 +1,60 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN 9
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+    
+void setup() {
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
+  strip.setBrightness(150);
+}
+
+void loop() {
+  scrollNewCol();
+}
+
+void scrollNewCol(){
+  for (int x = 0; x < 60; x++) {
+    strip.setPixelColor(x, 255,0,0);
+    delay(15);
+    strip.show();
+  }
+  for (int y = 60; y > 0; y--) {
+    strip.setPixelColor(y, 255,50,0);
+    delay(15);
+    strip.show();
+  }
+    for (int x = 0; x < 60; x++) {
+    strip.setPixelColor(x, 255,255,0);
+    delay(15);
+    strip.show();
+  }
+  for (int y = 60; y > 0; y--) {
+    strip.setPixelColor(y, 0,255,0);
+    delay(15);
+    strip.show();
+  }
+    for (int x = 0; x < 60; x++) {
+    strip.setPixelColor(x, 50,255,50);
+    delay(15);
+    strip.show();
+  }
+  for (int y = 60; y > 0; y--) {
+    strip.setPixelColor(y, 0,0,255);
+    delay(15);
+    strip.show();
+  }
+    for (int x = 0; x < 60; x++) {
+    strip.setPixelColor(x, 255,0,255);
+    delay(15);
+    strip.show();
+  }
+}
+
---
+++ Testing/nunchuckDemo/nunchuckDemo.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include "Nunchuk.h"
+
+void setup() {
+
+    Serial.begin(9600);
+    Wire.begin();
+    pinMode(13, OUTPUT);
+    //nunchuk_init_power(); // A1 and A2 is power supply
+    nunchuk_init();
+}
+
+void loop() {
+    Serial.println("Begin Loop");
+    if (nunchuk_read()) {
+      Serial.println("Inside if num read");
+        // Work with nunchuk_data
+        nunchuk_print();
+        Serial.println("end of num print command and if");
+    } else{
+      nunchuk_init();
+    digitalWrite(13,HIGH);
+    delay(1000);
+    digitalWrite(13,LOW);  
+    }
+    
+    } 
